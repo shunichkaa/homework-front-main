8e971249c0c97afb24ebda78debd3eeb910cb280
Aleksandra Mate <matealeksandra@gmail.com>
2025-05-15 18:52:46 +0200

    Homework week 4

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,14 @@
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string
+	setCurrentText: (value: string) => void
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
+	const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		props.setCurrentText(event.target.value);
 	};
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+	return (
+	  <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	);
 };
